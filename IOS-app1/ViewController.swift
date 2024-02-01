ee6c93cc1857661a44420270597975e4a75c8986
Mario Diaz <marioalan84@gmail.com>
2024-02-01 15:57:34 -0600

    New feature added! Text color changes

--- IOS-app1/ViewController.swift
+++ IOS-app1/ViewController.swift
@@ -9,12 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
     
-  
+    @IBOutlet weak var L1: UILabel!
+    @IBOutlet weak var L2: UILabel!
+    @IBOutlet weak var L3: UILabel!
+    
     
     @IBAction func changeBackgroundColor(_ sender: UIButton) {
-        let randomColor = changeColor();
-        view.backgroundColor = randomColor
-    }
+            let randomColor = changeColor()
+            view.backgroundColor = randomColor
+            
+            if isColorDark(color: randomColor) {
+                L1.textColor = .white
+                L2.textColor = .white
+                L3.textColor = .white
+            } else {
+                L1.textColor = .black
+                L2.textColor = .black
+                L3.textColor = .black
+            }
+        }
     
     func changeColor() -> UIColor{
 
@@ -24,6 +37,17 @@ class ViewController: UIViewController {
 
             return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
         }
+    
+    func isColorDark(color: UIColor) -> Bool {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        color.getRed(&red, green: &green, blue: &blue, alpha: nil)
+        
+        // Calculating the luminance of the color (formula found on internet)
+        let brightness = 0.299 * red + 0.587 * green + 0.114 * blue
+        return brightness < 0.5
+    }
     
     
     override func viewDidLoad() {
